c6c15648900b9e8cb97fa7d162eaa81b757dee28
Jason Zamorano <jhjzam@gmail.com>
2022-05-16 10:15:42 -0700

    Fill in dvars code

--- findoutlie/metrics.py
+++ findoutlie/metrics.py
@@ -2,7 +2,8 @@
 """
 
 #+ Your imports here.
-
+import numpy as np
+import nibabel as nib
 
 def dvars(img):
     """ Calculate dvars metric on Nibabel image `img`
@@ -21,11 +22,7 @@ def dvars(img):
         One-dimensional array with n-1 elements, where n is the number of
         volumnes in `img`.
     """
-    # Hint: remember 'axis='.  For example:
-    # In [2]: arr = np.array([[2, 3, 4], [5, 6, 7]])
-    # In [3]: np.mean(arr, axis=1)
-    # Out[2]: array([3., 6.])
-    #
-    # You may be be able to solve this in four lines, without a loop.
-    # But solve it any way you can.
-    return [42]
+    data = img.get_fdata()
+    d = data[..., 1:] - data[..., :-1]
+    result = np.sqrt(np.mean(d**2, axis=(0,1,2))) 
+    return result
